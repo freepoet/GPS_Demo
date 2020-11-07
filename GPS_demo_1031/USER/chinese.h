9929f773a4a5fb96997a0f93f6f2c87603b1c1c7
freepoet <ningmq@qq.com>
2020-11-07 12:22:47 +0800

    fixed some bugs

--- GPS_demo_1031/USER/chinese.h
+++
@@ -1,7 +0,0 @@
-#ifndef __CHINESE_H
-#define __CHINESE_H	 
-#include "stm32f4xx.h" 
-void TEST_FONT(void);
-void Test_Show_CH_Font16(u16 x,u16 y,u8 index,u16 color);
-void Test_Show_CH_Font24(u16 x,u16 y,u8 index,u16 color);		  				    
-#endif
